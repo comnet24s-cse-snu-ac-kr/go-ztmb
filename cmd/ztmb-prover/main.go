b02b17518b3d95b0e6c9b35a645c4631c027703f
haeramkeem <ewqdsacxz2345@gmail.com>
2024-06-15 09:32:51 +0900

    feat: udp server

--- cmd/ztmb-prover/main.go
+++ cmd/ztmb-prover/main.go
@@ -2,50 +2,87 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/hex"
 	"fmt"
 	"log"
+	"net"
 )
 
 const (
-  VRF_URL = "localhost"
-  VRF_PORT = 853
-  VRF_PROTOCOL = "tcp"
+  // TODO: Must be handled w/ env
+  UPSTREAM_ADDR = "localhost"
+  UPSTREAM_PORT = 853
+  UPSTREAM_PROTOCOL = "tcp"
+
+  SERVER_ADDR = "0.0.0.0"
+  SERVER_PORT = 53
+  SERVER_PROTOCOL = "udp"
 )
 
-func send(data []byte) ([]byte, error) {
+func upstream(data []byte) ([]byte, error) {
+  // 1. Connect TLS
 	config := &tls.Config{
 		InsecureSkipVerify: true,
 		MinVersion:         tls.VersionTLS13,
 	}
-
-	conn, err := tls.Dial(VRF_PROTOCOL, fmt.Sprintf("%s:%d", VRF_URL, VRF_PORT), config)
+	conn, err := tls.Dial(UPSTREAM_PROTOCOL, fmt.Sprintf("%s:%d", UPSTREAM_ADDR, UPSTREAM_PORT), config)
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
-  log.Println(fmt.Sprintf("Connected to verifier: %s:%d/%s", VRF_URL, VRF_PORT, VRF_PROTOCOL))
+  log.Printf("upstream: Connected (%s:%d/%s)", UPSTREAM_ADDR, UPSTREAM_PORT, UPSTREAM_PROTOCOL)
 
+  // 2. Send to upstream
 	if _, err = conn.Write(data); err != nil {
 		return nil, err
 	}
-  log.Println("Sent")
+  log.Printf("upstream: Sent (%s)", hex.EncodeToString(data))
 
+  // 3. Response
 	buf := make([]byte, 512)
 	n, err := conn.Read(buf)
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("Received")
+  log.Printf("upstream: Received (%s)", hex.EncodeToString(buf))
 
   return buf[:n], nil
 }
 
+func server() error {
+  // 1. Init
+	addr := net.UDPAddr{
+		Port: SERVER_PORT,
+		IP:   net.ParseIP(SERVER_ADDR),
+	}
+	conn, err := net.ListenUDP(SERVER_PROTOCOL, &addr)
+	if err != nil {
+    return err
+	}
+	defer conn.Close()
+  log.Printf("server: Started (%d/%s)", addr.Port, SERVER_PROTOCOL)
+
+	buffer := make([]byte, 1024)
+	for {
+    // 2. Handle request
+		n, remoteAddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+    log.Printf("server: Received (%d bytes, %v, %s)", n, remoteAddr, buffer[:n])
+
+    // 3. Response upstream
+    rcvd, err := upstream(buffer[:n])
+		_, err = conn.WriteToUDP(rcvd, remoteAddr)
+		if err != nil {
+			log.Printf("error: %v", err)
+			continue
+		}
+    log.Printf("server: Response (%d bytes ,%v, %s)", n, remoteAddr, buffer[:n])
+	}
+}
+
 func main() {
-  for {
-    var input string
-    if _, err := fmt.Scanln(&input); err != nil {
-      log.Fatal(err)
-    }
-    send([]byte(input))
-  }
+  server()
 }
